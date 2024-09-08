d7603e1f8be63877ceaf71e2812ec6b1335de908
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-09-08 20:37:57 -0300

    agregamos el componente Informacion con texto sobre react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Informacion from './components/Informacion'
 
 function App() {
 
   return (
 <main className='containter my-4'>
-  <h1>Primer Proyecto </h1>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br/>Nesciunt eius harum magnam earum rerum, nobis, <br></br> explicabo maxime perspiciatis eaque dicta consequatur officiis debitis exercitationem nisi quidem iste praesentium deleniti soluta.
-
+  <h1 class="text-center">Primer Proyecto </h1>
+  <p class="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br/>Nesciunt eius harum magnam earum rerum, nobis, <br></br> explicabo maxime perspiciatis eaque dicta consequatur officiis debitis exercitationem nisi quidem iste praesentium deleniti soluta.
   </p>
+  {/* invocar los componentes  */}
+  <Informacion></Informacion>
   </main>
   )
 }
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,24 @@
+const Informacion = () => {
+
+  // aqui agrego la mayor parte de logica
+
+  return (
+    // algo de logica
+    <section>
+      {/* aqui agrego comentario del maquetado*/}
+      <h2 class=" text-center">Introducion a react</h2>
+      <p class=" text-center">React es una biblioteca de JavaScript
+        desarrollada por Facebook para construir interfaces
+        de usuario. Se destaca por su enfoque en la 
+        creacion de aplicaciones web de una sola <strong>pagina SPA</strong>
+        y su capacidad para manejar la interfaz de manera 
+        eficiente mediante el uso de un <span className="parrafoResaltado">DOOM virtual</span>. React 
+        permite a los desarrolladores construir interfaces
+        dinamicas y reactivas de forma modular, lo que facilita el desarrollo y el
+        mantenimientoo de aplicaiones a gran escala.
+      </p>
+    </section>
+  );
+};
+
+export default Informacion;
