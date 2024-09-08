7c5b7829879e11083db37725f6a2824d9ad52fc8
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-09-08 10:27:54 -0300

    primer proyecto en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
-  <h1>Hola Mundo</h1>
+<main className='containter my-4'>
+  <h1>Primer Proyecto <React></React> </h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br/>Nesciunt eius harum magnam earum rerum, nobis, <br></br> explicabo maxime perspiciatis eaque dicta consequatur officiis debitis exercitationem nisi quidem iste praesentium deleniti soluta.
+
+  </p>
+  </main>
   )
 }
 
