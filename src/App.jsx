67aa57f84bff9e89f2dc416f5f4c4013b3fef477
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-09-08 10:32:00 -0300

    corregi un mal tipeo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
 <main className='containter my-4'>
-  <h1>Primer Proyecto <React></React> </h1>
+  <h1>Primer Proyecto </h1>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br/>Nesciunt eius harum magnam earum rerum, nobis, <br></br> explicabo maxime perspiciatis eaque dicta consequatur officiis debitis exercitationem nisi quidem iste praesentium deleniti soluta.
 
   </p>
